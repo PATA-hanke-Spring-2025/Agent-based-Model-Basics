4aeb69bc2348ee7c48e2e67267eda0f68d3e0634
Veyefill <nojusklimov@gmail.com>
2025-03-12 10:12:58 +0200

    Changes

--- agents.py
+++ agents.py
@@ -1,111 +1,29 @@
-import numpy as np
+import pandas as pd
+import random
 
 class Buyer:
     """
     Attributes:
         "Not Interested", "Evaluating", "Budgeting", "Deciding", "Go Nogo", "Delivered", "Satisfied"
     """
-    def __init__(self):
+    def __init__(self, transition_data, states):
         self.state = "Not Interested"
-
-        # Not Interested
-        self.NotInt_NotInt = 0.7
-        self.NotInt_Eval = 0.3
-        self.NotInt_Budget = 0.0
-        self.NotInt_Decide = 0.0
-        self.NotInt_GoNogo = 0.0
-        self.NotInt_Deliver = 0.0
-        self.NotInt_Satisf = 0.0
-        self.NotInt_Dissatisfied = 0.0 
-
-        # Evaluating
-        self.Eval_NotInt = 0.3
-        self.Eval_Eval = 0.3
-        self.Eval_Budget = 0.4
-        self.Eval_Decide = 0.0
-        self.Eval_GoNogo = 0.0
-        self.Eval_Deliver = 0.0
-        self.Eval_Satisf = 0.0
-        self.Eval_Dissatisfied = 0.0
-
-        # Budgeting
-        self.Budget_NotInt = 0.2
-        self.Budget_Eval = 0.2
-        self.Budget_Budget = 0.3
-        self.Budget_Decide = 0.3
-        self.Budget_GoNogo = 0.0
-        self.Budget_Deliver = 0.0
-        self.Budget_Satisf = 0.0
-        self.Budget_Dissatisfied = 0.0
-
-        # Deciding
-        self.Decide_NotInt = 0.1
-        self.Decide_Eval = 0.2
-        self.Decide_Budget = 0.1
-        self.Decide_Decide = 0.3
-        self.Decide_GoNogo = 0.3
-        self.Decide_Deliver = 0.0
-        self.Decide_Satisf = 0.0
-        self.Decide_Dissatisfied = 0.0
-
-        # Go_NOGO
-        self.GoNogo_NotInt = 0.0
-        self.GoNogo_Eval = 0.0
-        self.GoNogo_Budget = 0.0
-        self.GoNogo_Decide = 0.1
-        self.GoNogo_GoNogo = 0.2
-        self.GoNogo_Deliver = 0.7
-        self.GoNogo_Satisf = 0.0
-        self.GoNogo_Dissatisfied = 0.0
-
-        # Delivered
-        self.Deliver_NotInt = 0.0
-        self.Deliver_Eval = 0.0
-        self.Deliver_Budget = 0.0
-        self.Deliver_Decide = 0.0
-        self.Deliver_GoNogo = 0.0
-        self.Deliver_Deliver = 0.0 
-        self.Deliver_Satisf = 0.8
-        self.Deliver_Dissatisfied = 0.2
-
-        # Satisfied
-        self.Satisf_NotInt = 0.0
-        self.Satisf_Eval = 0.0
-        self.Satisf_Budget = 0.0
-        self.Satisf_Decide = 0.0
-        self.Satisf_GoNogo = 0.0
-        self.Satisf_Deliver = 0.0
-        self.Satisf_Satisfied = 0.9
-        self.Satisf_Dissatisfied = 0.1
-
-        # Dissatisfied 
-        self.Dissatisfied_NotInt = 0.0
-        self.Dissatisfied_Eval = 0.0
-        self.Dissatisfied_Budget = 0.0
-        self.Dissatisfied_Decide = 0.0
-        self.Dissatisfied_GoNogo = 0.0
-        self.Dissatisfied_Deliver = 0.0
-        self.Dissatisfied_Satisfied = 0.1
-        self.Dissatisfied_Dissatisfied = 0.9
-
+        self.transition_data = transition_data
+        self.states = states
         self.transition_matrix = self.create_initial_matrix()
 
     def create_initial_matrix(self):
-        return np.array ([
-            [self.NotInt_NotInt, self.NotInt_Eval, self.NotInt_Budget, self.NotInt_Decide, self.NotInt_GoNogo, self.NotInt_Deliver, self.NotInt_Satisf, self.NotInt_Dissatisfied],
-            [self.Eval_NotInt, self.Eval_Eval, self.Eval_Budget, self.Eval_Decide, self.Eval_GoNogo, self.Eval_Deliver, self.Eval_Satisf, self.Eval_Dissatisfied],
-            [self.Budget_NotInt, self.Budget_Eval, self.Budget_Budget, self.Budget_Decide, self.Budget_GoNogo, self.Budget_Deliver, self.Budget_Satisf, self.Budget_Dissatisfied],
-            [self.Decide_NotInt, self.Decide_Eval, self.Decide_Budget, self.Decide_Decide, self.Decide_GoNogo, self.Decide_Deliver, self.Decide_Satisf, self.Decide_Dissatisfied],
-            [self.GoNogo_NotInt, self.GoNogo_Eval, self.GoNogo_Budget, self.GoNogo_Decide, self.GoNogo_GoNogo, self.GoNogo_Deliver, self.GoNogo_Satisf, self.GoNogo_Dissatisfied],
-            [self.Deliver_NotInt, self.Deliver_Eval, self.Deliver_Budget, self.Deliver_Decide, self.Deliver_GoNogo, self.Deliver_Deliver, self.Deliver_Satisf, self.Deliver_Dissatisfied],
-            [self.Satisf_NotInt, self.Satisf_Eval, self.Satisf_Budget, self.Satisf_Decide, self.Satisf_GoNogo, self.Satisf_Deliver, self.Satisf_Satisfied, self.Satisf_Dissatisfied],
-            [self.Dissatisfied_NotInt, self.Dissatisfied_Eval, self.Dissatisfied_Budget, self.Dissatisfied_Decide, self.Dissatisfied_GoNogo, self.Dissatisfied_Deliver, self.Dissatisfied_Satisfied, self.Dissatisfied_Dissatisfied]
-        ])
-    
+        matrix = []
+        for from_state in self.states['State']:
+            row = []
+            for to_state in self.states['State']:
+                value = self.transition_data.loc[self.transition_data['From/To'] == from_state, to_state].values[0]
+                value = float(value.replace(',', '.'))
+                row.append(value)
+            matrix.append(row)
+        return pd.DataFrame(matrix, index=self.states['State'], columns=self.states['State'])
+
     def step(self):
-        states = ["Not Interested", "Evaluating", "Budgeting", "Deciding", "Go Nogo", "Delivered", "Satisfied", "Dissatisfied"]
-        current_state_index = states.index(self.state)
-        probabilities = self.transition_matrix[current_state_index]
-        next_state_index = np.random.choice(range(len(states)), p = probabilities)
-        self.state = states[next_state_index]
+        probabilities = self.transition_matrix.loc[self.state].values
+        self.state = random.choices(self.states['State'].tolist(), weights=probabilities)[0]
         return self.state
--- app.py
+++ app.py
@@ -1,41 +1,14 @@
-import ipywidgets as widgets
-from IPython.display import display
+import pandas as pd
 from agents import Buyer
 from model import Model
 
-buyer = Buyer()
+transition_data = pd.read_csv("SellerTransition.csv", delimiter=";")
+states = pd.read_csv("BuyerStates.csv")
+
+buyer = Buyer(transition_data, states)
 model = Model(buyer)
 
-sliders = {}
-for attr in dir(buyer):
-    if attr.startswith(('NotInt_', 'Eval_', 'Budget_', 'Decide_', 'GoNogo_', 'Deliver_', 'Satisf_', 'Dissatisfied_')):
-        sliders[attr] = widgets.FloatSlider(value=getattr(buyer, attr), min=0, max=1, step=0.01, description=attr)
-
-output = widgets.Output()
-step_button = widgets.Button(description="Take Step")
-
-def updateProb(change) :
-    newProbs = {attr: slider.value for attr, slider in sliders.items()}
-    model.update_transition_matrix(**newProbs)
-    with output:
-        output.clear_output()
-        print("Updated Matrix")
-        print(buyer.transition_matrix)
-        print(f"Initial State: {buyer.state}")
-
-def take_step(b):
+num_steps = 5
+for _ in range(num_steps):
+    print(f"Current State: {buyer.state}")
     model.step()
-    with output:
-        print(f"State Updated: {buyer.state}")
-
-for slider in sliders.values():
-    slider.observe(updateProb, names='value')
-
-step_button.on_click(take_step)
-
-for slider in sliders.values():
-    display(slider)
-display(step_button)
-display(output)
-
-updateProb(None)
