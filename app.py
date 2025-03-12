96ae76e8c195b5744ffaf09db4b141e7c400e893
Veyefill <nojusklimov@gmail.com>
2025-03-12 10:22:02 +0200

    Fixed .csv reading

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from agents import Buyer
 from model import Model
 
 transition_data = pd.read_csv("SellerTransition.csv", delimiter=";")
-states = pd.read_csv("BuyerStates.csv")
+states = pd.read_csv("BuyerStates.csv", delimiter=";")
 
 buyer = Buyer(transition_data, states)
 model = Model(buyer)
 
-num_steps = 5
-for _ in range(num_steps):
-    print(f"Current State: {buyer.state}")
+step_count = 0
+while buyer.state not in ["Satisfied", "Dissatisfied"]:
+    print(f"Current state: {buyer.state}")
     model.step()
+    step_count += 1
+
+print(f"Final State: {buyer.state}")
+print(f"Steps Taken: {step_count}")
